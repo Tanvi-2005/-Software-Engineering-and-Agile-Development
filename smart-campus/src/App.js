e39a49a39357fafaa48282f25dc4674ea45677c6
Tanvi-2005 <btanviBTECH23@ced.alliance.edu.in>
2026-03-31 20:11:59 +0530

    Update App.js

--- smart-campus/src/App.js
+++ smart-campus/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Smart Campus Portal 🚀</h1>
-      <p>Now it is working correctly!</p>
+    <div>
+      <Navbar />
+      <Home />
     </div>
   );
 }
